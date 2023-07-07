785bb684ec721b38051c953cd24210abee84a7bd
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2023-07-07 11:46:18 +0100

    Ignore react and render strings directly

--- src/components/Anonimize/Content.tsx
+++ src/components/Anonimize/Content.tsx
@@ -4,6 +4,7 @@ import AnonimizeBlock from './Block';
 import AnonimizeTooltip from './Tooltip';
 import { UserFile } from '../../types/UserFile';
 import { useEntities, useImages, useSpecificOffsets, useTypes, useTypesDict } from '../../util/uses';
+import { renderBlock } from './render';
 
 interface AnonimizeContentProps {
     file: UserFile
@@ -20,17 +21,22 @@ export default function AnonimizeContent(props: AnonimizeContentProps){
     const entityTypes = useTypesDict(props.file);
     const images = useImages(props.file)
 
+    const html = useMemo( () => renderBlock(props.file.doc, entityTypes, offsets, props.anonimizeState, 0), [props.file.doc, props.anonimizeState, entityTypes, offsets])
+    const {accessHtml, anonimizeState} = props;
+    
     useEffect(() => {
+        accessHtml(html)
         nodesRef.current = Array.from(contentRef.current?.querySelectorAll(`[data-offset]`) as NodeListOf<HTMLElement>)
-        props.accessHtml(contentRef.current?.innerHTML || "");
+    },[html, accessHtml])
 
+    useEffect(() => {
         let imagesElm = Array.from(contentRef.current?.getElementsByTagName("img") as HTMLCollectionOf<HTMLImageElement>)
 
         imagesElm.forEach((img,i) =>{
             img.dataset.imageId = i.toString()
             img.dataset.bsToggle = "modal"
             img.dataset.bsTarget = "#modal-image-editor"
-            if( props.anonimizeState !== AnonimizeStateState.ORIGINAL){
+            if( anonimizeState !== AnonimizeStateState.ORIGINAL){
                 if( images[i] && images[i].anonimizedSrc ){
                     img.src = images[i].anonimizedSrc!
                 }
@@ -42,14 +48,12 @@ export default function AnonimizeContent(props: AnonimizeContentProps){
                 img.src = images[i].originalSrc
             }
         })
-    }, [props.anonimizeState, images])
+    }, [anonimizeState, images])
 
 
-    
     return <>
-        <div id="content" className={props.showTypes ? 'show-type' : 'show-cod'} ref={contentRef}>
-            <AnonimizeBlock element={props.file.doc} offset={0} types={entityTypes} specificOffsets={offsets} anonimizeState={props.anonimizeState}/>
-        </div>
+        <div id="content" className={props.showTypes ? 'show-type' : 'show-cod'} ref={contentRef} dangerouslySetInnerHTML={{__html: html}}></div>
         <AnonimizeTooltip entityTypes={Object.values(entityTypes)} pool={props.file.pool} contentRef={contentRef} nodesRef={nodesRef} />
     </>
 }
+
