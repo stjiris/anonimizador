9b39437246b484e50dcaf1b7bd4595595df7df06
ist1111237 <francisco.b.santos@tecnico.ulisboa.pt>
2025-07-24 15:49:50 +0100

    Fixing bugs in the filters.

--- python-cli/specific_spacy.py
+++ python-cli/specific_spacy.py
@@ -334,7 +334,7 @@ def merge(ents, text):
 def nlp(text, model):
     model.add_pipe("new_line_segmenter", before="ner")
     # Ordering functions
-    model.add_pipe("find_addresses", before="remove_entities_with_excluded_words")
+    # model.add_pipe("find_addresses", before="remove_entities_with_excluded_words")
     model.add_pipe("remove_entities_with_excluded_words", last=True)
     # Create entity list
     ents = []
