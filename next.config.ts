25ce55a66770f0a29277f2ddcba79931b0b4b4c8
Miguel Fonseca <miguel.dias.fonseca@gmail.com>
2026-01-09 17:22:59 +0000

    nginx issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  basePath: '/anon-service',
 };
 
 export default nextConfig;
