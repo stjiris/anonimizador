044fa30583ee53846403818afae52cf0aa04f4c3
Miguel Fonseca <miguel.dias.fonseca@gmail.com>
2026-01-11 17:15:45 +0000

    debug flag for browser

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
