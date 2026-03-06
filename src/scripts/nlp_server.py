397ea5c06f64348084508dd9912781a7b79aef04
vascofelix <vascorfelix@tecnico.ulisboa.pt>
2026-03-06 15:17:22 +0000

    changed to eduardo's newest model

--- src/scripts/nlp_server.py
+++ src/scripts/nlp_server.py
@@ -4,7 +4,7 @@ import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 print("Loading model...", flush=True)
-model = spacy.load("./src/scripts/model-gpt/model-best")
+model = spacy.load("./src/scripts/model-gpt/model-extended")
 print("Model loaded.", flush=True)
 
 class Handler(BaseHTTPRequestHandler):
